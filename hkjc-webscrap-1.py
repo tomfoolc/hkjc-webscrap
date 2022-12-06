cd8faf16eb48025b592794cb86e8b6fad66b8e71
tomfoolc <foolc@yahoo.com>
2022-12-06 15:43:51 +0800

    To-do for getting racing page

--- hkjc-webscrap-1.py
+++ hkjc-webscrap-1.py
@@ -12,30 +12,72 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 
-from selenium.webdriver.common.by import By
-
+#from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 import time
 
-#define Chrome Options
+#################################
+# define constant variable
+#################################
+_BraveBrowserPath = r'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe'
+_HkjcRaceResultUrl = r'https://racing.hkjc.com/racing/information/Chinese/Racing/LocalResults.aspx'
+_HkjcRaceResultRaceDateParam = r'?RaceDate='
+_HkjcRaceResultRaceNoParam = r'&RaceNo='
+
+#################################
+# define function
+#################################
+#get the URL of HJKC race result page
+def getHkjcRaceResultUrl(raceYear=0, raceMonth=0, raceDay=0, raceNo=0):
+    url = _HkjcRaceResultUrl
+    if (raceYear != 0 and raceMonth != 0 and raceDay != 0 and raceNo != 0):
+        url += _HkjcRaceResultParam + raceYear + r'/' + raceMonth + r'/' + raceDay + _HkjcRaceResultRaceDateParam + raceNo
+    return url
+
+#get the racing date
+def getRaceDate(_chrome):
+    raceDateItems = []
+    
+    _chrome.get(getHkjcRaceResultUrl()) #get the init page
+    soup = BeautifulSoup(chrome.page_source, 'html.parser')
+
+    raceDateList = soup.select('#selectId') #select the code with id=selectId
+
+    if (len(raceDateList) > 0):
+        raceDateItems = raceDateList[0].findAll('option')
+
+    return raceDateItems
+
+
+#########################################
+# define Chrome Options and Web Driver
+#########################################
 #binary_location: Brave Browser location (for opening with Brave Browser)
-#add_argument("--disable-notifications"): diable pop-up / notification
+#add_argument("--disable-notifications"): disable pop-up / notification
 options = webdriver.ChromeOptions()
-options.binary_location = r'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe'
+options.binary_location = _BraveBrowserPath
 options.add_argument("--disable-notifications")
 
-#driver is placed on the upper folder and named as chromedriver108
 #selenium4 changed to use selenium.webdriver.chrome.service to initial webdriver
 #ref: http://pypi.org/project/webdriver-manager
 #chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) #Chrome
 chrome = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()), options=options) #Brave
 
-chrome.get("https://racing.hkjc.com/racing/information/Chinese/Racing/LocalResults.aspx")
+##################
+# get Racing Date
+##################
+raceDateItems = getRaceDate(chrome)
 
-time.sleep(3)
+# To-do
+# 1. assemble the race date and get the race result page
+# 2. cross check the race date with the input race date
+# 3. cross check with the input date for stopping the web scraping
+# 4. determine valid racing result
+# 4.1 exclude Typhoon day (2-Nov-2022) 
+# 4.2 exclude non-HK racing (1-Nov-2022)
+#
 
-raceDateList = chrome.find_element(By.ID, "selectId")
-raceDateItems = raceDateList.find_elements(By.TAG_NAME, "option")
 
 for raceDateItem in raceDateItems:
     raceDate = raceDateItem.text
